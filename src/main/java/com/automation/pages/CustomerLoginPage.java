e662d0464dc2b63f4ef7a5b9f2f239bc44237046
REGENCY-14 <osmantimtonizakaria14@gmail.com>
2026-02-23 11:29:49 +0000

    Fix test failures and update CI pipeline with notifications

--- src/main/java/com/automation/pages/CustomerLoginPage.java
+++ src/main/java/com/automation/pages/CustomerLoginPage.java
@@ -1,10 +1,14 @@
 package com.automation.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,10 +18,11 @@ import org.slf4j.LoggerFactory;
 public class CustomerLoginPage {
     
     private static final Logger logger = LoggerFactory.getLogger(CustomerLoginPage.class);
-    private WebDriver driver;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
     
     // Web Elements
-    @FindBy(name = "userSelect")
+    @FindBy(id="userSelect")
     private WebElement customerDropdown;
     
     @FindBy(xpath = "//button[contains(text(), 'Login')]")
@@ -25,17 +30,22 @@ public class CustomerLoginPage {
     
     public CustomerLoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         PageFactory.initElements(driver, this);
     }
     
     public void selectCustomer(String customerName) {
         logger.info("Selecting customer: {}", customerName);
+        wait.until(ExpectedConditions.elementToBeClickable(customerDropdown));
         customerDropdown.click();
-        driver.findElement(By.xpath("//option[text()='" + customerName + "']")).click();
+        WebElement option = driver.findElement(By.xpath("//option[text()='" + customerName + "']"));
+        wait.until(ExpectedConditions.elementToBeClickable(option));
+        option.click();
     }
     
     public void clickLogin() {
         logger.info("Clicking login button");
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         loginButton.click();
     }
     
